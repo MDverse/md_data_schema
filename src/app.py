db0bb4f36c3b50ddd7ddd9571f519b502a449f02
GiuliaDGM <giuliadgm05@gmail.com>
2025-02-03 13:34:26 +0100

    Cleaning database files

--- src/app.py
+++ src/app.py
@@ -2,7 +2,15 @@
 Purpose: Create your main app and call SQLModel.metadata.create_all() in app.py
 """
 
-from sqlmodel import SQLModel, create_engine
-from . import db
+from .db import create_db_and_tables, engine
+from .models import *
+
+def main():
+    create_db_and_tables()
+    # create_heroes()
+
+
+if __name__ == "__main__":
+    main()
 
 # SQLModel.metadata.create_all(engine)
