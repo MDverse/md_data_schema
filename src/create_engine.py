3a0b36cd7be3eecc9b81d26d347882761b9df7f1
GiuliaDGM <giuliadgm05@gmail.com>
2025-02-18 17:11:24 +0100

    Updating tables from model import

--- src/create_engine.py
+++ src/create_engine.py
@@ -3,26 +3,31 @@ Purpose: Create the engine object in a file db.py
 """
 
 from sqlmodel import SQLModel, create_engine
-from models import (Author,
-                    Dataset,
-                    DatasetOrigin,
-                    File,
-                    FileType,
-                    TopologyFile,
-                    ParameterFile,
-                    TrajectoryFile,
-                    Thermostat,
-                    Barostat,
-                    Integrator,
-                    Molecule,
-                    MoleculeExternalDb,
-                    MoleculeType,
-                    Database,
-                    DatasetAuthorLink,
-                    DatasetMoleculeLink,
-                    MoleculeTopologyLink)
 
-sqlite_file_name = "database.db"
+from models import (
+    Author,  # noqa: F401
+    Barostat,  # noqa: F401
+    Database,  # noqa: F401
+    Dataset,  # noqa: F401
+    DatasetAuthorLink,  # noqa: F401
+    DatasetKeywordLink,  # noqa: F401
+    DatasetMoleculeLink,  # noqa: F401
+    DatasetOrigin,  # noqa: F401
+    File,  # noqa: F401
+    FileType,  # noqa: F401
+    Integrator,  # noqa: F401
+    Keyword,  # noqa: F401
+    Molecule,  # noqa: F401
+    MoleculeExternalDb,  # noqa: F401
+    MoleculeTopologyLink,  # noqa: F401
+    MoleculeType,  # noqa: F401
+    ParameterFile,  # noqa: F401
+    Thermostat,  # noqa: F401
+    TopologyFile,  # noqa: F401
+    TrajectoryFile,  # noqa: F401
+)
+
+sqlite_file_name = "database_test.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url)
